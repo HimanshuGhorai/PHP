4994370fd960b834cfd1d47eb06683a71652f0c9
Himanshu <140635492+HimanshuGhorai@users.noreply.github.com>
2025-10-16 20:31:29 +0530

    add name variable for test to commit

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+
+echo 'hi this is varibles.php';
+echo '<br>';
+echo 'hi this is varibles.php';
+echo '<br>'; 
+
+$name = "himanshu";
+$age = 23;
+echo "my name is $name and my age is $age";// so we are iusing double quotes so that it can print the value of variables
+echo '<br>';
+
+?>
