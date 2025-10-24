222bc0519ddab52e61f9f438f3df4b74b7fac967
Himanshu <140635492+HimanshuGhorai@users.noreply.github.com>
2025-10-24 16:13:01 +0530

    add some datatypes

---
+++ constant.php
@@ -0,0 +1,9 @@
+<?php
+// $name = 'himu';
+
+const newname = 'himanshu';
+// newname = 'ghorai';// we are not assign to value to const
+// echo newname;
+define("datano","himanshu"); // this anther way to write const
+echo datano;
+?>
---
+++ datatype.php
@@ -0,0 +1,50 @@
+<?php
+
+echo 'All Data Type';
+echo '<br/>';
+
+
+
+// String Dara type 
+// A - Z all
+$Name = "himu";
+echo var_dump($Name);
+
+echo '<br/>';
+
+
+// Integer Data type
+// 0 - 9
+
+$Num = 1234;
+echo var_dump($Num);
+
+echo '<br/>';
+
+
+// float Data type
+// 00.00
+$floatnum= 19.43;
+
+echo var_dump($floatnum);
+echo '<br/>';
+
+
+// Boolean 
+
+$ismale = true;
+
+echo var_dump($ismale);
+
+
+// Null 
+
+$empty= null;
+echo var_dump($empty);
+// Array
+
+// Object(class)
+
+// Recourse
+
+?>
