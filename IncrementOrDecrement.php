64f82167e80b95c2815fb349368ea9f0b1582802
Himanshu <140635492+HimanshuGhorai@users.noreply.github.com>
2025-11-17 19:27:40 +0530

    increment and decrement operator

---
+++ IncrementOrDecrement.php
@@ -0,0 +1,14 @@
+
+<?php
+$x = 5;
+++$x; // this is for first increment the value and then use it ;
+echo $x;
+
+$x++;// this is for first print and next time useit ;
+
+echo "<br/>";
+echo $x; 
+
+
+
+?>
