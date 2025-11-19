c744227536fa6335ada550e85f8d60686368eca7
Himanshu <140635492+HimanshuGhorai@users.noreply.github.com>
2025-11-19 18:32:21 +0530

    string in php

---
+++ string.php
@@ -0,0 +1,22 @@
+<?php 
+// A string is a sequence of characters like text, numbers, or symbols.
+// "Hello"
+// "12345"
+// "Hello World!"
+// "PHP is great 😊"
+// In PHP, strings are written inside:
+
+// Double quotes (" ")
+
+// Single quotes (' ')
+
+
+
+$name = 'himanshu';
+$title = 'ghorai';
+
+var_dump($name);
+echo gettype($name);
+
+
+?>
